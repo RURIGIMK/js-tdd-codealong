54e866fbc205735f73fcdf1f5cadb31d97f2ad11
RURIGIMK <rurigimk@gmail.com>
2024-06-12 18:54:33 +0300

    Commit

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,9 +1,16 @@
+// src/__tests__/utils.test.js
 import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
+    // Arrange
     const birthYear = 1984;
-    const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+    const expectedAge = new Date().getFullYear() - birthYear;
+
+    // Act
+    const actualAge = currentAgeForBirthYear(birthYear);
+
+    // Assert
+    expect(actualAge).toBe(expectedAge);
   });
 });
